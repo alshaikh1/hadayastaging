c0cc700b58b4c401bb3ec77fde5be44145d7b9df
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-09-20 01:00:02 +0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="section wb main">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -64,5 +65,6 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
