41fab048a327063b89b6971872c093195ec969c4
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-09-20 01:20:32 +0300

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content=""> 
 	<meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Site Icons -->
     <link rel="shortcut icon" href="{{ secure_asset('images/favicon.ico') }}" type="image/x-icon" />
