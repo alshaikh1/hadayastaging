8854d0dbe04d251d3aa04da61f6d1ca0dcf83dfe
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-09-28 23:08:56 +0300

    staging

--- resources/views/pages/add.blade.php
+++ resources/views/pages/add.blade.php
@@ -22,8 +22,133 @@
 			<!-- end sidebar -->
 			</div>
 			
-			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-				hey
+			<div class="col-sm-9 col-md-10 main">
+				<div id="demo">
+					@foreach ($Products as $Product)
+						@if ($Product->cat === 'wedding')
+						<div class="col-md-3 col-sm-3 col-xs-12 productslist">
+							<div class="service-widget">
+								<div class="post-media wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
+									<a href="#" id="morebutton{{ $Product->id }}" data-toggle="modal" data-target="#modal{{ $Product->id }}">
+										<img src="{{ secure_asset($Product->productimage) }}" alt="" class="img-responsive img-rounded">
+									</a>
+								</div>
+								<h3>
+									<a href="#" id="morebutton{{ $Product->id }}" data-toggle="modal" data-target="#modal{{ $Product->id }}">{{ $Product->productname }}</a>
+								</h3>
+								<h4>{{ $Product->productprice }}
+										@if ($Product->productprice < 10)
+										دنانير
+										@else
+										دينار
+										@endif
+								</h4>
+								<h4>مقدم من {{ $Product->merchantname }}<h4>
+								<p>{{ str_limit($Product->productdescription, $limit = 51, $end = '...') }}</p>					
+								<a href="Javascript:Void(0);" id="morebutton{{ $Product->id }}" type="button" data-toggle="modal" data-target="#modal{{ $Product->id }}">المزيد</a>
+								<div class="checkbox">
+									<label class="btn">
+										<input type="checkbox" id="addproduct{{ $Product->id }}" value="{{ $Product->id }}" name="addproductcheckbox[]"> اضافة
+									</label>
+								</div>
+							<!-- end service -->
+							</div>
+							<div class="modal fade bs-example-modal-lg" id="modal{{ $Product->id }}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+								<div class="modal-dialog modal-lg">
+									<div class="modal-content">
+										<div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+											<h4 class="modal-title" id="exampleModalLabel">{{ $Product->productname }}</h4>
+										</div>
+										<div class="modal-body row">
+											<div class="modalimg col-md-6 col-sm-12 col-xs-12">
+												<img src="{{ secure_asset($Product->productimage) }}" alt="" class="img-responsive img-rounded">
+											</div>
+											<div class="col-md-6 col-sm-12 col-xs-12 text-right">
+												<h4>السعر: {{ $Product->productprice }} 
+												@if ($Product->productprice < 10)
+												دنانير
+												@else
+												دينار
+												@endif </h4>
+												<h4>المتجر: {{ $Product->merchantname }}</h4>
+												<h4>التوصيل: مجاني</h4>
+												<p>
+												<b>المواصفات:</b> <br />{{ $Product->productdescription }}
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						<!-- end col -->
+						</div>
+					@endif
+					@endforeach
+					</div>
+					<div id="demo2" style="display:none">
+						@foreach ($Products as $Product)
+						@if ($Product->cat === 'baby')
+						<div class="col-md-3 col-sm-3 col-xs-12 productslist">
+							<div class="service-widget">
+								<div class="post-media wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
+									<a href="#" id="morebutton{{ $Product->id }}" data-toggle="modal" data-target="#modal{{ $Product->id }}">
+										<img src="{{ secure_asset($Product->productimage) }}" alt="" class="img-responsive img-rounded">
+									</a>
+								</div>
+								<h3>
+									<a href="#" id="morebutton{{ $Product->id }}" data-toggle="modal" data-target="#modal{{ $Product->id }}">{{ $Product->productname }}</a>
+								</h3>
+								<h4>{{ $Product->productprice }} 
+									@if ($Product->productprice < 10)
+									دنانير
+									@else
+									دينار
+									@endif
+								</h4>
+								<h4>مقدم من {{ $Product->merchantname }}<h4>
+								<p>{{ str_limit($Product->productdescription, $limit = 51, $end = '...') }}</p>
+								<a href="Javascript:Void(0);" id="morebutton{{ $Product->id }}" type="button" data-toggle="modal" data-target="#modal{{ $Product->id }}">المزيد</a>
+								<div class="checkbox">
+									<label class="btn">
+										<input type="checkbox" id="addproduct{{ $Product->id }}" value="{{ $Product->id }}" name="addproductcheckbox[]"> اضافة
+									</label>
+								</div>
+							<!-- end service -->
+							</div>
+							<div class="modal fade bs-example-modal-lg" id="modal{{ $Product->id }}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+								<div class="modal-dialog modal-lg">
+									<div class="modal-content">
+										<div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+											<h4 class="modal-title" id="exampleModalLabel">{{ $Product->productname }}</h4>
+										</div>
+										<div class="modal-body row">
+											<div class="modalimg col-md-6 col-sm-12 col-xs-12">
+												<img src="{{ secure_asset($Product->productimage) }}" alt="" class="img-responsive img-rounded">
+											</div>
+											<div class="col-md-6 col-sm-12 col-xs-12 text-right">
+												<h4>السعر: {{ $Product->productprice }} 
+													@if ($Product->productprice < 10)
+													دنانير
+													@else
+													دينار
+													@endif </h4>
+												<h4>المتجر: {{ $Product->merchantname }}</h4>
+												<h4>التوصيل: مجاني</h4>
+												<p>
+													<b>المواصفات:</b> <br />{{ $Product->productdescription }}
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						<!-- end col -->
+						</div>
+					@endif
+					@endforeach
+					</div>
 			<!-- end col-9 -->
 			</div>
 		<!-- end row-->	
