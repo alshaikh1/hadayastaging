49db178a7ed10f249b0e9857d123d06929092bd4
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-09-28 22:27:23 +0300

    staging

--- resources/views/pages/add.blade.php
+++ resources/views/pages/add.blade.php
@@ -8,15 +8,15 @@
 	</div>
 	
 	<div class="container">
-	
-	@if($success != null)
-		<div class="alert alert-success" role="alert">$success</div>
-	@endif
 		
-	
-	
 		<form class="needs-validation form-horizontal" novalidate="" action="{{ secure_url('/add') }}" method="POST" id="formvalid">
 		{{ csrf_field() }}
+		
+		@if($success != null)
+			<div class="alert alert-success" role="alert">$success</div>
+		@endif
+		
+		
 			<ul class="nav nav-tabs" role="tablist">
 				<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">تفاصيل المناسبة</a></li>
 				<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">اختيار الهدايا</a></li>
