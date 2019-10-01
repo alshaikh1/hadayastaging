6724400ebcfc7bfc4f39b2d9218e1e765304ca8f
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-10-01 22:18:32 +0300

    auth

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -40,7 +40,6 @@ class PagesController extends Controller
 	
 	public function add(Request $request){
         $title = 'اضافة مناسبة';
-		$this->middleware('auth');
 		$success = '';
 		$cat = $request->query('cat');
 		$Products = DB::table('hd_products')->where('cat', '=', $cat)->paginate(10)->appends(request()->except('page'));
