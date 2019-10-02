b83e433e3eca22d6f10a3f5e6a4a7b6924f93af7
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-10-02 22:43:07 +0300

    new add

--- resources/views/pages/viewcart.blade.php
+++ resources/views/pages/viewcart.blade.php
@@ -9,9 +9,9 @@
 	<div class="container">
 		<div class="row">
 			@foreach(session('occasion') as $ids => $occasion)
-				<table>
+				<table class="table table-striped table-hover text-right">
 					<tr>
-						<td>{{ $occasion['occdate'] }}</td>
+						<td>{{ $occasion=>occdate }}</td>
 						<td>{{ $occasion['firstname'] }}</td>
 						<td>{{ $occasion['lastname'] }}</td>
 						<td>{{ $occasion['emailaddress'] }}</td>
