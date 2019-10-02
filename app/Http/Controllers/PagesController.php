ed4991782bfa8837906f51180de4ce6578ec0c30
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-10-02 21:32:29 +0300

    new add

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -64,8 +64,8 @@ class PagesController extends Controller
 			'occdate' => 'required|date',
 			'firstname' => 'required',
 			'lastname' => 'required',
-            'emailaddress' => 'required|email|unique:hd_occasions, emailaddress',
-			'mobilenumber' => 'required|numeric|exists:hd_occasions, mobilenumber',
+            //'emailaddress' => 'required|email|unique:hd_occasions, emailaddress',
+			'mobilenumber' => 'required|numeric|unique:hd_occasions, mobilenumber',
 			'description' => 'required',
 			'address' => 'required'
 		]);
