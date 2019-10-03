5be37362ee0637605ba61e1ed2577b730a8f3e07
Mohammad Al Shaikh <m.alshaikh@umniah.com>
2019-10-03 19:49:51 +0300

    new cart

--- resources/views/pages/addstep2.blade.php
+++ resources/views/pages/addstep2.blade.php
@@ -162,7 +162,7 @@
  
             if(confirm("Are you sure")) {
                 $.ajax({
-                    url: '{{ url('remove-from-cart') }}',
+                    url: '{{ secure_url('remove-from-cart') }}',
                     method: "DELETE",
                     data: {_token: '{{ csrf_token() }}', id: ele.attr("data-id")},
                     success: function (response) {
